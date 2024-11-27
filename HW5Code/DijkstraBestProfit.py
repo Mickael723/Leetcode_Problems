cf4d1ed3b10cfcefe9331a25f238ca49f1e5d590
Mickael Osegueda <mickaelosegueda031@gmail.com>
2024-11-27 15:42:03 -0800

    MST fully operational

--- HW5Code/DijkstraBestProfit.py
+++ HW5Code/DijkstraBestProfit.py
@@ -1,53 +1,45 @@
 import heapq
 import math
-from collections import defaultdict
+from collections import deque
 
 if __name__== "__main__":
 
     # Get input
     num_cities, num_roads = tuple(map(int, input().split()))
     city_selling_prices = []
-    adjacency_list = defaultdict(list)
     for i in range(num_cities):
         city_selling_prices.append(int(input()))
-    # Using an adjacency list for simplicity
+
+    # Using an edge list for simplicity
+    edges = []
     for i in range(num_roads):
         city_x, city_y, cost = tuple(map(int, input().split()))
         # Need to add each edge twice for bidirectionality
-        adjacency_list[city_x].append((city_y, cost))
-        adjacency_list[city_y].append((city_x, cost))
-    
+        edges.append((city_x, city_y, cost))
+        edges.append((city_y, city_x, cost))
+    edges.sort()
+
     # Perform Dijkstra
     tentative_distances = [math.inf] * num_cities
     tentative_distances[0] = 0
     visited_nodes = set()
     queue = []
+    edges = deque(edges)
     heapq.heappush(queue, 0)
     while len(queue) != 0:
         current_node = heapq.heappop(queue)
         visited_nodes.add(current_node)
-        for path in adjacency_list[current_node]:
-            if path[0] not in visited_nodes:
-                tentative_distances[path[0]] = min(tentative_distances[path[0]], 
-                                               tentative_distances[current_node] + path[1])
-                heapq.heappush(queue, path[0])
-
+        for city_x, city_y, cost in edges:
+            if current_node == city_x and city_y not in visited_nodes:
+                tentative_distances[city_y] = min(tentative_distances[city_y], 
+                                               tentative_distances[city_x] + cost)
+                heapq.heappush(queue, city_y)
+        edges.popleft() # Shorten the list of edges to parse through on the next run
+ 
     # Calculate the best profit
     max_profit = 0
     for i in range(num_cities):
         profit = city_selling_prices[i] - 2 * tentative_distances[i]
         max_profit = max(max_profit, profit)
-
+ 
     print(max(0, max_profit))
-
-
-    
-    
-
-
-
-
-
-    
-    
-    
