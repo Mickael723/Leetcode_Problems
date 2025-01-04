ccbeeea485ae0483de6e01f41be39a5619b10f2f
Mickael Osegueda <mickaelosegueda031@gmail.com>
2025-01-04 13:03:13 -0800

    LCSNoRepeats Solved.

--- Leetcode/LCSNoRepeats.py
+++ Leetcode/LCSNoRepeats.py
@@ -1,31 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        solutions = [-1] * (len(s) + 1)
-        def LLS(i: int) -> int:
-            if i == 0: return 0
-            if solutions[i] != -1:
-                return solutions[i]
-            
-            seen_chars = set()
-            curr_longest_length = 0
-            prev_longest_length = 1
+        seen_chars = set()
+        curr_longest_length = 0
+        left_ptr = 0
+        right_ptr = 0
 
-            for j in range(i):
-                if s[j] in seen_chars:
-                    prev_longest_length = curr_longest_length
-                    curr_longest_length = 0
-                    seen_chars.clear()
-                    
-                curr_longest_length += 1
-                seen_chars.add(s[j])
-                    
+        while right_ptr < len(s):
+            if s[right_ptr] not in seen_chars:
+                seen_chars.add(s[right_ptr])
+                curr_longest_length = max(curr_longest_length, len(seen_chars))
+                right_ptr += 1
+            else:
+                seen_chars.remove(s[left_ptr])
+                left_ptr += 1
 
-            computed_longest_length = max(prev_longest_length, curr_longest_length)
-            solutions[i] = max(computed_longest_length, LLS(i - 1))
+        return curr_longest_length
 
-        for i in range(0, len(s) + 1):
-            LLS(i)
-        return max(max(solutions), 0)
 
 
 if __name__== "__main__":
@@ -33,5 +23,7 @@ if __name__== "__main__":
     print(sol.lengthOfLongestSubstring("abcabcbb"))  # Output: 3
     print(sol.lengthOfLongestSubstring("bbbbb"))     # Output: 1
     print(sol.lengthOfLongestSubstring("pwwkew"))    # Output: 3
+    print(sol.lengthOfLongestSubstring(""))          # Output: 0
     print(sol.lengthOfLongestSubstring(" "))         # Output: 1
     print(sol.lengthOfLongestSubstring("a b c"))     # Output: 3
+    print(sol.lengthOfLongestSubstring("dvdf"))      # Output: 3
